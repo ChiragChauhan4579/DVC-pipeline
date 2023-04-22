7b445e7c9e154a289bbd0a5290c95505a07d88f3
Chauhan Chirag <chiragchauhan1030@gmail.com>
2023-04-22 16:31:04 +0530

    Created train and test CSV files

--- src/prepare.py
+++ src/prepare.py
@@ -1,39 +1,23 @@
 from pathlib import Path
-
 import pandas as pd
-
-FOLDERS_TO_LABELS = {"n03445777": "golf ball", "n03888257": "parachute"}
-
-
-def get_files_and_labels(source_path):
-    images = []
-    labels = []
-    for image_path in source_path.rglob("*/*.JPEG"):
-        filename = image_path.absolute()
-        folder = image_path.parent.name
-        if folder in FOLDERS_TO_LABELS:
-            images.append(filename)
-            label = FOLDERS_TO_LABELS[folder]
-            labels.append(label)
-    return images, labels
-
-
-def save_as_csv(filenames, labels, destination):
-    data_dictionary = {"filename": filenames, "label": labels}
-    data_frame = pd.DataFrame(data_dictionary)
-    data_frame.to_csv(destination)
-
 
 def main(repo_path):
     data_path = repo_path / "data"
     train_path = data_path / "raw/train"
     test_path = data_path / "raw/val"
-    train_files, train_labels = get_files_and_labels(train_path)
-    test_files, test_labels = get_files_and_labels(test_path)
-    prepared = data_path / "prepared"
-    save_as_csv(train_files, train_labels, prepared / "train.csv")
-    save_as_csv(test_files, test_labels, prepared / "test.csv")
 
+    train_df = pd.read_csv(train_path / "train.csv")
+    test_df = pd.read_csv(test_path / "test.csv")
+
+    train_df.drop(columns=["id"], inplace=True)
+    test_df.drop(columns=["id"], inplace=True)
+    train_df.rename(columns={"M": 0,"B":1}, inplace=True)
+    test_df.rename(columns={"M": 0,"B":1}, inplace=True)
+
+    prepared = data_path / "prepared"
+
+    train_df.to_csv(prepared / "train.csv", index=False)
+    test_df.to_csv(prepared / "test.csv", index=False)
 
 if __name__ == "__main__":
     repo_path = Path(__file__).parent.parent
